3eb04ce8245b8048347e9a924783d5ab45bdbf2e
Naiara Garcia <ngarcia091@ikasle.ehu.eus>
2015-12-30 13:57:48 +0100

    Hasta contenido multimedia incluido

---
+++ app/src/main/java/es/tta/ejemploclase/AudioPlayer.java
@@ -0,0 +1,127 @@
+package es.tta.ejemploclase;
+
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.MediaController;
+
+import java.io.IOException;
+
+public class AudioPlayer implements MediaController.MediaPlayerControl, MediaPlayer.OnPreparedListener
+{
+    private View view;
+    private MediaPlayer player;
+    private MediaController controller;
+
+    public AudioPlayer(View view, final Runnable onExit)
+    {
+        this.view = view;
+        player = new MediaPlayer();
+        player.setOnPreparedListener(this);
+        controller = new MediaController(view.getContext())
+        {
+            @Override
+            public void hide()
+            {}
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event)
+            {
+                if(event.getKeyCode()==KeyEvent.KEYCODE_BACK)
+                {
+                    release();
+                    onExit.run();
+                }
+                return super.dispatchKeyEvent(event);
+            }
+        };
+    }
+
+    public void setAudioUri(Uri uri) throws IOException
+    {
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setDataSource(view.getContext(), uri);
+        player.prepare();
+        player.start();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp)
+    {
+        controller.setMediaPlayer(this);
+        controller.setAnchorView(view);
+        controller.show(0);
+    }
+
+    @Override
+    public void start()
+    {
+        player.start();
+    }
+
+    @Override
+    public void pause()
+    {
+        player.pause();
+    }
+
+    public void release()
+    {
+        if(player!=null)
+        {
+            player.stop();
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public int getDuration() {
+        return player.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return player.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        player.seekTo(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return player.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return (player.getCurrentPosition()*100)/player.getDuration();
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return player.getAudioSessionId();
+    }
+
+
+}
--- app/src/main/java/es/tta/ejemploclase/TestActivity.java
+++ app/src/main/java/es/tta/ejemploclase/TestActivity.java
@@ -1,21 +1,35 @@
 package es.tta.ejemploclase;
 
+import android.provider.Settings;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.audiofx.BassBoost;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 public class TestActivity extends AppCompatActivity
 {
+    private String advise = "<h1>My First Heading</h1> <p>My first paragraph.</p>";
+    //private String advise = "http://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,9 +46,79 @@ public class TestActivity extends AppCompatActivity
         }
     }
 
-    public void enviar(View view)
+    //private void showHtml(String advise)
+    public void showHtml(View view)//SUSTITUIR POR LA DE ARRIBA ¡OJO! Tiene que ser public
     {
+        if(advise.substring(0,10).contains("://"))
+        {
+            Toast.makeText(this, "ENTRO EN PRIMER CASO", Toast.LENGTH_SHORT).show();
+            Uri uri =Uri.parse(advise);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "ENTRO EN SEGUNDO CASO", Toast.LENGTH_SHORT).show();
+            WebView web = new WebView(this);
+            web.loadData(advise, "text/html", null);
+            web.setBackgroundColor(Color.TRANSPARENT);
+            web.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+            LinearLayout layout = (LinearLayout)findViewById(R.id.test_layout);
+            layout.addView(web);
+        }
+    }
 
+    //private void showVideo(String advise)
+    public void showVideo(View view)
+    {
+        VideoView video = new VideoView(this);
+        //video.setVideoURI(Uri.parse(advise));
+        video.setVideoURI(Uri.parse("http://www.semanticdevlab.com/abc.mp4"));
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        video.setLayoutParams(params);
+
+        MediaController controller = new MediaController(this)
+        {
+            @Override
+            public void hide()
+            {}
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event)
+            {
+                if(event.getKeyCode()==KeyEvent.KEYCODE_BACK)
+                {
+                    finish();
+                }
+                return super.dispatchKeyEvent(event);
+            }
+        };
+        controller.setAnchorView(video);
+        video.setMediaController(controller);
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.test_layout);
+        layout.addView(video);
+        video.start();
+    }
+
+    public void playAudio(View view)
+    {
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+        LinearLayout layout_audio = (LinearLayout)findViewById(R.id.audio_layout);
+        AudioPlayer ap = new AudioPlayer(layout_audio, run);
+        try
+        {
+            ap.setAudioUri(Settings.System.DEFAULT_RINGTONE_URI);
+        }
+        catch(Exception e)
+        {
+
+        }
     }
 
 }
